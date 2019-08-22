b63d2137ca9c8d575df61f9f3e93c6aa680335f4
Alexander Kulik <a.kulik@dodopizza.com>
2019-08-22 21:27:05 +0300

    Added autofac.

--- src/DatabaseProjects/Database.Base/RemboardContextFactory.cs
+++ src/DatabaseProjects/Database.Base/RemboardContextFactory.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
+using Autofac;
 using Common;
 using Common.Data;
+using Common.Extensions;
 using Common.Features;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
@@ -17,9 +20,21 @@ namespace Database.Base
         {
             var optionsBuilder = new DbContextOptionsBuilder<RemboardContext>();
             //try configure from here https://garywoodfine.com/configuration-api-net-core-console-application/
-            optionsBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;Initial Catalog=Remboard2;Integrated Security=True;MultipleActiveResultSets=true");
-            IEnumerable<IConfigureModelFeature> resolvers = new IConfigureModelFeature[] {new CommonModule(), new UsersModule()};
-            return new RemboardContext(optionsBuilder.Options, resolvers);
+            optionsBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;Initial Catalog=Remboard2;Integrated Security=True;MultipleActiveResultSets=true",m=>m.MigrationsAssembly(typeof(RemboardContextFactory).Assembly.FullName));
+
+            var builder = new ContainerBuilder();
+            var refAssembyNames = typeof(RemboardContextFactory).Assembly
+                .GetReferencedAssemblies();
+
+            foreach (var refAssembyName in refAssembyNames)
+            {
+                var loadedAssembly = Assembly.Load(refAssembyName);
+                builder.RegisterAssemblyTypes(loadedAssembly).Where(t => t.HasImplementation<IConfigureModelFeature>())
+                    .As<IConfigureModelFeature>();
+            }
+
+            builder.RegisterInstance(optionsBuilder.Options);
+            return builder.Build().Resolve<RemboardContext>();
         }
     }
 }
